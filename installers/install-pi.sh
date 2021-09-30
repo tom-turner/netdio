aaf289f8f8164a74cff42644849fa8cdceabcd38
tom-turner <72453954+tom-turner@users.noreply.github.com>
2021-09-30 12:26:18 +0100

    raspotify script added

--- installers/install-pi.sh
+++ installers/install-pi.sh
@@ -1,3 +1,5 @@
+sudo apt update
+sudo apt upgrade
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 nvm install stable
 npm install pm2 -g
--- installers/install-raspotify-pi.sh
+++ installers/install-raspotify-pi.sh
@@ -1,1 +1,22 @@
 curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
+sudo tee -a ~/.config/systemd/user/raspotify.service > /dev/null <<EOT
+[Unit]
+Description=Raspotify
+Wants=pulseaudio.service
+
+
+[Service]
+Restart=always
+RestartSec=10
+Environment="DEVICE_NAME=Duckado"                                                               
+Environment="BITRATE=160"
+Environment="CACHE_ARGS=--disable-audio-cache"
+Environment="VOLUME_ARGS=--enable-volume-normalisation --linear-volume --initial-volume=90"
+Environment="BACKEND_ARGS=--backend alsa"
+EnvironmentFile=-/etc/default/raspotify
+ExecStart=/usr/bin/librespot --name \${DEVICE_NAME} \$BACKEND_ARGS --bitrate \${BITRATE} \$CACHE_ARGS \$VOLUME_ARGS \$OPTIONS
+
+[Install]      
+WantedBy=default.target
+EOT
+systemctl --user enable raspotify.service
