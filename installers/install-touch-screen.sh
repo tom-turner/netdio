285b62616600bf07752ccb763b9d6d26cd77af2c
tom-turner <72453954+tom-turner@users.noreply.github.com>
2021-09-30 11:41:06 +0100

    Update install-touch-screen.sh

--- installers/install-touch-screen.sh
+++ installers/install-touch-screen.sh
@@ -1,6 +1,7 @@
+sudo apt update
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
 sudo apt-get install --no-install-recommends chromium-browser
-sudo apt-get install --no-install-recommends chromium
+sudo apt-get install --no-install-recommends chromium chromium-l10n
 sudo tee -a /etc/xdg/openbox/autostart > /dev/null <<EOT
 xset -dpms            # turn off display power management system
 xset s noblank        # turn off screen blanking
