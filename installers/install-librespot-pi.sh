4224a00b1f0da66085367640377fd7e9d5799d0a
tom-turner <72453954+tom-turner@users.noreply.github.com>
2021-09-30 23:48:05 +0100

    added no resampling for pi performance

---
+++ installers/install-librespot-pi.sh
@@ -0,0 +1,11 @@
+cd ~/
+curl https://sh.rustup.rs -sSf | sh
+echo export PATH="\$HOME/.cargo/bin:\$PATH" >> ~/.bash_profile
+rustup component add rustfmt
+sudo apt-get install build-essential
+sudo apt-get instatll libpulse-dev
+git clone https://github.com/librespot-org/librespot.git
+cd librespot
+cargo build --no-default-features --features "alsa-backend" --release
+cd ~/librespot/target/debug
+./librespot -n "Duck" -b 320 --enable-volume-normalisation
--- installers/install-raspotify-pi.sh
+++
@@ -1,2 +0,0 @@
-curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
-# need to manually edit sudo systemctl daemon-reload
