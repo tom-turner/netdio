01dcff02d326a95f6d4076bd234172dd1a5b363c
tom-turner <72453954+tom-turner@users.noreply.github.com>
2021-09-30 12:14:10 +0100

    Update install-touch-screen.sh

--- installers/install-touch-screen.sh
+++ installers/install-touch-screen.sh
@@ -1,4 +1,5 @@
 sudo apt update
+sudo apt upgrade
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
 sudo apt-get install --no-install-recommends chromium-browser
 sudo apt-get install --no-install-recommends chromium chromium-l10n
@@ -19,8 +20,9 @@ chromium --start-fullscreen --kiosk --incognito --noerrdialogs --disable-transla
 --check-for-update-interval=31536000
 EOT
 sudo tee -a ~/.bash_profile > /dev/null <<EOT
-[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor
+[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && startx -- -nocursor
 EOT
+source ~/.bash_profile
 sudo tee -a /boot/config.txt > /dev/null <<EOT
 lcd_rotate=2 
 EOT
