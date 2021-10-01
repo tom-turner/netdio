12ae248a835d7fc268f043ae2db5ecf9ff782412
tom-turner <72453954+tom-turner@users.noreply.github.com>
2021-10-01 14:17:17 +0100

    Update install-librespot-pi.sh

--- installers/install-librespot-pi.sh
+++ installers/install-librespot-pi.sh
@@ -1,12 +1,12 @@
 cd ~/
 curl https://sh.rustup.rs -sSf | sh
-sudo tee -a ~/.bash_profile > /dev/null <<EOT
-export PATH="\$HOME/.cargo/bin:\$PATH"
-EOT
 rustup component add rustfmt
 sudo apt-get install build-essential
 sudo apt-get install libpulse-dev
-git clone https://github.com/librespot-org/librespot.git
-cd librespot
+git clone https://github.com/librespot-org/librespot.git ~/
+sudo tee -a ~/.bash_profile > /dev/null <<EOT
+export PATH="\$HOME/.cargo/bin:\$PATH"
+EOT
+cd ~/librespot
 cargo build --no-default-features --features "pulseaudio-backend" --release
 ~/librespot/target/release/librespot -n "Duck" -b 320 --initial-volume 95 --enable-volume-normalisation --normalisation-pregain "-3"
