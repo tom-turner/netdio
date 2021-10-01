c264b5f1a5e222ab0119352168eca2335d76552b
tom-turner <72453954+tom-turner@users.noreply.github.com>
2021-10-01 14:37:34 +0100

    fixing install scripts

--- installers/install-librespot-pi.sh
+++ installers/install-librespot-pi.sh
@@ -1,12 +1,14 @@
-cd ~/
 sudo curl https://sh.rustup.rs -sSf | sh
-sudo rustup component add rustfmt
+sudo tee -a ~/.bash_profile > /dev/null <<EOT
+export PATH="~/.cargo/bin:"
+EOT
+source ~/.bash_profile
+rustup install stable
+rustup default stable
+rustup component add rustfmt
 sudo apt-get install build-essential
 sudo apt-get install libpulse-dev
-git clone https://github.com/librespot-org/librespot.git ~/
-sudo tee -a ~/.bash_profile > /dev/null <<EOT
-export PATH="\$HOME/.cargo/bin:\$PATH"
-EOT
+git clone https://github.com/librespot-org/librespot.git ~/librespot
 cd ~/librespot
 cargo build --no-default-features --features "pulseaudio-backend" --release
 ~/librespot/target/release/librespot -n "Duck" -b 320 --initial-volume 95 --enable-volume-normalisation --normalisation-pregain "-3"
--- installers/install-touch-screen.sh
+++ installers/install-touch-screen.sh
@@ -31,8 +31,7 @@ duck ALL=NOPASSWD:/sbin/reboot
 EOT
 sudo rm -r ~/netdio/config/startupconfig.json 
 sudo tee -a ~/netdio/config/startupconfig.json > /dev/null <<EOT 
-{"tx":{"name":"Spotify", "type":"tx"},
-"device":{"color":"#ADDDD8","colordark":"#85b5b0","name":"Duckado"},
+{"device":{"color":"#ADDDD8","colordark":"#85b5b0","name":"Duckado"},
 "source":{"name":"-Mute-"}}
 EOT
 sudo reboot
