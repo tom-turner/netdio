6df6aca9dd2ce2354594149f58ed32821412bbcf
tom-turner <72453954+tom-turner@users.noreply.github.com>
2021-10-11 19:57:45 +0100

    Update install-amp.sh

--- installers/install-amp.sh
+++ installers/install-amp.sh
@@ -1,8 +1,8 @@
 sudo rm -r ~/netdio/config/startupconfig.json 
 sudo rm -r ~/netdio/config/config.json 
 sudo tee -a ~/netdio/config/startupconfig.json > /dev/null <<EOT 
-{"rx":{"name":"AMP Out","type":"rx","volume":"80","driver":"alsa","hardware":"hw:sndrpihifiberry,0"},
-"device":{"color":"#ADDDD8","colordark":"#85b5b0","name":"EtherDAC"},
+{"rx":{"name":"EtherDAC AMP","type":"rx","volume":"80","driver":"alsa","hardware":"hw:sndrpihifiberry,0"},
+"device":{"color":"#ADDDD8","colordark":"#85b5b0","name":"EtherDAC AMP"},
 "source":{"name":"-Mute-"}}
 EOT
 sudo tee -a ~/.asoundrc > /dev/null <<EOT 
