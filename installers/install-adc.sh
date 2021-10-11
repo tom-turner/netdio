27d11d3e13bb2780d0dec90a2b03f8e413ec1581
tom-turner <72453954+tom-turner@users.noreply.github.com>
2021-10-11 18:46:13 +0100

    Update install-adc.sh

--- installers/install-adc.sh
+++ installers/install-adc.sh
@@ -2,7 +2,7 @@ sudo rm -r ~/netdio/config/startupconfig.json
 sudo rm -r ~/netdio/config/config.json 
 sudo tee -a ~/netdio/config/startupconfig.json > /dev/null <<EOT 
 {"rx":{"name":"EtherDAC Out","type":"rx","volume":"80","driver":"alsa","hardware":"hw:sndrpihifiberry,0"},
-"tx":{"name":"EtherDAC In", "type":"tx", "driver":"alsa","hardware":"hw:sndrpihifiberry,1"},
+"tx":{"name":"EtherDAC In", "type":"tx", "driver":"alsa","hardware":"hw:sndrpihifiberry,0"},
 "device":{"color":"#ADDDD8","colordark":"#85b5b0","name":"EtherDAC"},
 "source":{"name":"-Mute-"}}
 EOT
