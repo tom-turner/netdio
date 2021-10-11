d88a734f67a1325b986bd4d616cbd087285b1076
tom-turner <72453954+tom-turner@users.noreply.github.com>
2021-10-11 19:57:06 +0100

    amp update

--- installers/install-amp.sh
+++ installers/install-amp.sh
@@ -1,8 +1,16 @@
 sudo rm -r ~/netdio/config/startupconfig.json 
 sudo rm -r ~/netdio/config/config.json 
 sudo tee -a ~/netdio/config/startupconfig.json > /dev/null <<EOT 
-{"rx":{"name":"EtherDAC Out","type":"rx","volume":"80","driver":"alsa","hardware":"hw:sndrpihifiberry,0"},
+{"rx":{"name":"AMP Out","type":"rx","volume":"80","driver":"alsa","hardware":"hw:sndrpihifiberry,0"},
 "device":{"color":"#ADDDD8","colordark":"#85b5b0","name":"EtherDAC"},
 "source":{"name":"-Mute-"}}
 EOT
+sudo tee -a ~/.asoundrc > /dev/null <<EOT 
+defaults.pcm.card 1
+defaults.ctl.card 1
+EOT
 sh configure-hifiberry.sh
+pm2 start ~/netdio/index.js
+pm2 startup 
+# run the output of startup
+pm2 save
--- installers/install-duckado-pi.sh
+++ installers/install-duckado-pi.sh
@@ -14,7 +14,6 @@ nvm install stable
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 sudo apt-get install build-essential
-# sudo apt-get install libjack-dev -y
 sudo apt-get install libasound2-dev -y
 git clone https://github.com/librespot-org/librespot.git ~/librespot
 cargo ~/librespot/build --no-default-features --features "alsa-backend" --release
