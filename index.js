bb4fbc25493878202e1f6805514de724e792de69
tom-turner <72453954+tom-turner@users.noreply.github.com>
2021-10-22 18:43:07 +0100

    test

--- index.js
+++ index.js
@@ -73,7 +73,6 @@ var devices = new Devices(config)
 devices.receive('discovery', (device) => {
   devices.addDevice(device)
 })
-devices.publish()
 devices.receive('ctrl message', (message) => {
     switch (message.type) {
       case 'source':
