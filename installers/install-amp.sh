0c1d4175dce9e2028669ec0579be878e295b1305
tom-turner <72453954+tom-turner@users.noreply.github.com>
2021-11-02 13:41:30 +0000

    swap to dmix

--- installers/install-amp.sh
+++ installers/install-amp.sh
@@ -1,7 +1,7 @@
 sudo rm -r ~/netdio/config/startupconfig.json 
 sudo rm -r ~/netdio/config/config.json 
 sudo tee -a ~/netdio/config/startupconfig.json > /dev/null <<EOT 
-{"rx":{"name":"EtherDAC AMP","type":"rx","volume":"80","driver":"alsa","hardware":"hw:sndrpihifiberry,0"},
+{"rx":{"name":"EtherDAC AMP","type":"rx","volume":"80","driver":"alsa","hardware":"dmix:sndrpihifiberry,0"},
 "device":{"color":"#ADDDD8","colordark":"#85b5b0","name":"EtherDAC AMP"},
 "source":{"name":"-Mute-"}}
 EOT
