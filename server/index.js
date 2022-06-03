102d99160c5bbb9bcbb04a33ad1af10ea5ba5761
tom-turner <tom.turner@live.co.uk>
2022-06-03 11:05:50 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,17 +10,10 @@ const routes = require("./routes");
 const config = require('./lib/config')();
 const { Tx, Rx, Spotify } = require('./lib/networkServices')
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-}
-
- app.use(allowCrossDomain);
-
-
+app.use(cors({
+  origin: (origin, next) => next(null, origin),
+  credentials: true
+}))
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.urlencoded());
